cc6e55d315033661243bf275307a837ee3382aed
Somi <backsomi@naver.com>
2024-02-27 01:55:13 +0900

    init commit

---
+++ src/main/java/KGUcapstone/OutDecision/OutDecisionApplication.java
@@ -0,0 +1,13 @@
+package KGUcapstone.OutDecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutDecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutDecisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KGUcapstone/OutDecision/OutDecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package KGUcapstone.OutDecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutDecisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
