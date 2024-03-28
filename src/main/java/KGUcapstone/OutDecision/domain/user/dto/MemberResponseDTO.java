6b7f17ed91fefc7f7c7f44c5521312df990049e8
sye1101 <shinye1101@naver.com>
2024-03-28 15:15:26 +0900

    [REFACTOR]: DTO 클래스명 변경

--- src/main/java/KGUcapstone/OutDecision/domain/user/dto/MemberResponseDTO.java
+++ src/main/java/KGUcapstone/OutDecision/domain/user/dto/MemberResponseDTO.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class MemberResponseDTO {
+
+    // 마이페이지 개인정보수정에서 사용되는 개인정보 DTO
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class MemberDTO{
+    public static class MemberInfoDTO{
         Long memberId;
         String name;
         String email;
