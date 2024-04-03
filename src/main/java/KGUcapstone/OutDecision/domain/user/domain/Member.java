8d49f68bf542e9c1315a6953cd941c16bb73de7f
baeksom <97323941+baeksom@users.noreply.github.com>
2024-04-03 17:12:33 +0900

    🎨 [UPDATE]: 사용자 엔티티 수정

--- src/main/java/KGUcapstone/OutDecision/domain/user/domain/Member.java
+++ src/main/java/KGUcapstone/OutDecision/domain/user/domain/Member.java
@@ -21,8 +21,7 @@ public class Member extends BaseEntity {
 
     @Column(nullable = false, length = 50)
     private String email;
-
-    @Column(length = 20)
+    git push 
     private String password;
 
     @Column(length = 20)
