3d84ed1d8aff963bd72b3042bf894e8efe88e0da
sye1101 <shinye1101@naver.com>
2024-04-11 00:53:43 +0900

    [FEAT]: Redis 관련 세부 설정

---
+++ src/main/java/KGUcapstone/OutDecision/global/config/redis/RedisConfig.java
@@ -0,0 +1,95 @@
+package KGUcapstone.OutDecision.global.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
+//
+//package KGUcapstone.OutDecision.global.config.redis;
+//
+//        import lombok.RequiredArgsConstructor;
+//        import org.springframework.beans.factory.annotation.Value;
+//        import org.springframework.context.annotation.Bean;
+//        import org.springframework.context.annotation.Configuration;
+//        import org.springframework.data.redis.connection.RedisConnectionFactory;
+//        import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//        import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//        import org.springframework.data.redis.core.RedisTemplate;
+//        import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+//
+//@Configuration
+//@EnableRedisRepositories
+//@RequiredArgsConstructor
+//public class RedisConfig {
+//
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//    @Value("${spring.redis.password}")
+//    private String redisPassword;
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        redisStandaloneConfiguration.setHostName(redisHost);
+//        redisStandaloneConfiguration.setPort(redisPort);
+//        redisStandaloneConfiguration.setPassword(redisPassword);
+//
+//        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+//    }
+//
+//    @Bean
+//    public RedisTemplate<?, ?> redisTemplate() {
+//
+//        // redisTemplate 를 받아와서 set, get, delete 를 사용
+//        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+//        /*
+//         * setKeySerializer, setValueSerializer 설정
+//         * redis-cli 을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
+//         */
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//
+//        return redisTemplate;
+//    }
+//
+//}
