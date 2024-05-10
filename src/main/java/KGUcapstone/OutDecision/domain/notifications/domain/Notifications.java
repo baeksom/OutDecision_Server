d246809a88983f2a6a345d0f29ff497be3d8f66d
sye1101 <shinye1101@naver.com>
2024-05-11 03:14:30 +0900

    [FEAT]: 게시글 알림 엔티티 구현

---
+++ src/main/java/KGUcapstone/OutDecision/domain/notifications/domain/Notifications.java
@@ -0,0 +1,26 @@
+package KGUcapstone.OutDecision.domain.notifications.domain;
+
+import KGUcapstone.OutDecision.domain.post.domain.Post;
+import KGUcapstone.OutDecision.domain.user.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"post_id", "member_id"}))
+public class Notifications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
