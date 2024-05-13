c26a9b2a2cb1282448adc55a4ff922476b4011ae
han <gksxofkd1234@naver.com>
2024-05-13 20:20:15 +0900

    [FEAT] 추천게시글 API

--- src/main/java/KGUcapstone/OutDecision/domain/post/service/PostsService.java
+++ src/main/java/KGUcapstone/OutDecision/domain/post/service/PostsService.java
@@ -2,7 +2,6 @@ package KGUcapstone.OutDecision.domain.post.service;
 
 import KGUcapstone.OutDecision.domain.post.domain.Post;
 import org.springframework.data.domain.Page;
-
 import java.util.Map;
 
 public interface PostsService {
@@ -14,4 +13,7 @@ public interface PostsService {
                                Integer page,
                                Integer size);
 
+    public Page<Post> getRecommendPost(Long memberId,
+                                       Integer page,
+                                       Integer size);
 }
