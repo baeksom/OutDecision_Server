9463afa17125ca1f3f719bc95f7f556a244733c9
baeksom <97323941+baeksom@users.noreply.github.com>
2024-05-24 20:17:01 +0900

    [UPDATE]: 로그인 쿠키 수정

--- src/main/java/KGUcapstone/OutDecision/domain/user/controller/TokenController.java
+++ src/main/java/KGUcapstone/OutDecision/domain/user/controller/TokenController.java
@@ -3,6 +3,7 @@ package KGUcapstone.OutDecision.domain.user.controller;
 import KGUcapstone.OutDecision.domain.user.service.FindMemberService;
 import KGUcapstone.OutDecision.domain.user.service.auth.TokenService;
 import KGUcapstone.OutDecision.global.error.exception.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import static KGUcapstone.OutDecision.global.common.util.CookieUtil.addCookie;
+import static KGUcapstone.OutDecision.global.common.util.CookieUtil.deleteCookie;
 
 @Slf4j
 @RestController
@@ -21,28 +25,25 @@ public class TokenController {
     private final FindMemberService findMemberService;
 
     @PostMapping("/token/logout")
-    public ApiResponse<Object> logout() {
+    public ApiResponse<Object> logout(HttpServletResponse response) {
         String accessToken = findMemberService.getTokenFromCookies();
         // 엑세스 토큰으로 현재 Redis 정보 삭제
         tokenService.removeRefreshToken(accessToken);
+        // 쿠키에서 토큰 삭제
+        deleteCookie(response, "accessToken");
         return ApiResponse.onSuccess(null);
     }
 
     @PostMapping("/token/refresh")
-    public ResponseEntity<ApiResponse<Object>> refresh() {
+    public ResponseEntity<ApiResponse<Object>> refresh(HttpServletResponse response) {
         String accessToken = findMemberService.getTokenFromCookies();
         String newAccessToken = tokenService.republishAccessToken(accessToken);
         if (StringUtils.hasText(newAccessToken)) {
+            // 클라이언트에게 응답할 때 쿠키를 변경한다.
+            addCookie(response, "accessToken", newAccessToken, 60*60);
             return ResponseEntity.ok(ApiResponse.onSuccess(newAccessToken));
         }
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.onFailure("TOKEN400", "Access Token 발급에 실패했습니다.", null));
     }
-
-//    // 토큰 만료를 확인하기 위한 테스트 컨트롤러
-//    @PostMapping("/test")
-//    public ApiResponse<Object> test() {
-//        return ApiResponse.onSuccess(null);
-//    }
-
 }
