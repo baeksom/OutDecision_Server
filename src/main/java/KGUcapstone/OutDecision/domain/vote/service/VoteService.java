8eae24d76f5a1993618198331b69f25857e26fa3
baeksom <97323941+baeksom@users.noreply.github.com>
2024-05-28 01:31:21 +0900

    🎨 [UPDATE]: 투표 수정

--- src/main/java/KGUcapstone/OutDecision/domain/vote/service/VoteService.java
+++ src/main/java/KGUcapstone/OutDecision/domain/vote/service/VoteService.java
@@ -1,7 +1,10 @@
 package KGUcapstone.OutDecision.domain.vote.service;
 
+import KGUcapstone.OutDecision.domain.post.dto.PostsResponseDTO;
+import KGUcapstone.OutDecision.domain.vote.dto.VoteResponseDto;
+
 import java.util.List;
 
 public interface VoteService {
-    public boolean addVote(List<Long> optionsIds);
+    VoteResponseDto.VoteResultDTO addVote(List<Long> optionsIds);
 }
