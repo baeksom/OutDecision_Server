73e8552b43f61d97de7d7b48d74a35c803344f08
baeksom <97323941+baeksom@users.noreply.github.com>
2024-05-29 11:55:20 +0900

    🎨 [UPDATE]: 댓글 작성 수정

---
+++ src/main/java/KGUcapstone/OutDecision/domain/comments/converter/CommentsConverter.java
@@ -0,0 +1,27 @@
+package KGUcapstone.OutDecision.domain.comments.converter;
+
+import KGUcapstone.OutDecision.domain.comments.domain.Comments;
+import KGUcapstone.OutDecision.domain.comments.dto.CommentsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static KGUcapstone.OutDecision.global.util.DateTimeFormatUtil.formatCreatedAt2;
+
+@Component
+@RequiredArgsConstructor
+public class CommentsConverter {
+
+    public CommentsResponseDto toCommentsResponseDto(Comments comments){
+
+        return CommentsResponseDto.builder()
+                .id(comments.getId())
+                .body(comments.getBody())
+                .nickname(comments.getMember().getNickname())
+                .postId(comments.getPost().getId())
+                .profileUrl(comments.getMember().getUserImg())
+                .createdAt(formatCreatedAt2(comments.getCreatedAt()))
+                .isOwn(true)
+                .build();
+    }
+
+}
