48b52db512193fe58c13119f47c360846e0ff547
sye1101 <shinye1101@naver.com>
2024-05-31 12:02:40 +0900

    [UPDATE]: 칭호 이모지 추가

--- src/main/java/KGUcapstone/OutDecision/domain/title/repository/TitleRepository.java
+++ src/main/java/KGUcapstone/OutDecision/domain/title/repository/TitleRepository.java
@@ -28,16 +28,16 @@ public interface TitleRepository extends JpaRepository<Title, Long> {
 
     @Query("SELECT " +
             "GROUP_CONCAT(" +
-            "   CASE WHEN t.ceo = true THEN '사장' ELSE '' END, " +
-            "   CASE WHEN t.fashionista = true THEN ',패셔니스타' ELSE '' END, " +
-            "   CASE WHEN t.foodie = true THEN ',미식가' ELSE '' END, " +
-            "   CASE WHEN t.greedy = true THEN ',욕심쟁이' ELSE '' END, " +
-            "   CASE WHEN t.romantist = true THEN ',로맨티스트' ELSE '' END, " +
-            "   CASE WHEN t.sprout = true THEN ',새싹' ELSE '' END, " +
-            "   CASE WHEN t.traveler = true THEN ',트래블러' ELSE '' END, " +
-            "   CASE WHEN t.first = true THEN ',1위' ELSE '' END, " +
-            "   CASE WHEN t.second = true THEN ',2위' ELSE '' END, " +
-            "   CASE WHEN t.third = true THEN ',3위' ELSE '' END " +
+            "   CASE WHEN t.ceo = true THEN '💸사장' ELSE '' END, " +
+            "   CASE WHEN t.fashionista = true THEN ',🕶️패셔니스타' ELSE '' END, " +
+            "   CASE WHEN t.foodie = true THEN ',🍴미식가' ELSE '' END, " +
+            "   CASE WHEN t.greedy = true THEN ',😏욕심쟁이' ELSE '' END, " +
+            "   CASE WHEN t.romantist = true THEN ',💖로맨티스트' ELSE '' END, " +
+            "   CASE WHEN t.sprout = true THEN ',🌱새싹' ELSE '' END, " +
+            "   CASE WHEN t.traveler = true THEN ',✈️트래블러' ELSE '' END, " +
+            "   CASE WHEN t.first = true THEN ',🥇1위' ELSE '' END, " +
+            "   CASE WHEN t.second = true THEN ',🥈2위' ELSE '' END, " +
+            "   CASE WHEN t.third = true THEN ',🥉3위' ELSE '' END " +
             ") AS true_titles " +
             "FROM Title t " +
             "WHERE t.member.id = :memberId " +
